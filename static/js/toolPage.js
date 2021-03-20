1dd147dffe486c0d113c839d676b2498adbc97a9
TNumFive <determinetof@gmail.com>
2021-03-20 11:33:04 +0800

    修改input框的位置，修改id为随机的uuid

--- static/js/toolPage.js
+++ static/js/toolPage.js
@@ -14,12 +14,13 @@ function uuidv4() {
  */
 function copyToClipboard(text) {
     //采用uuid作为id，避免不知道啥情况导致的id重复
-    $('body').append('<div><input id="3a4ef5238d61494bab6cb33bfbdbb962" readonly></div>');
-    var input = $('#3a4ef5238d61494bab6cb33bfbdbb962');
+    var uuid=uuidv4();
+    $('body').after('<input id="'+uuid+'" readonly>');
+    var input = $('#'+uuid);
     input.val(text);
     input.select();
     //注意：execCommand('copy')的前提是选中可编辑区域，且该可编辑区域未被隐藏
-    return document.execCommand('copy') | input.parent().remove();
+    return document.execCommand('copy') | input.remove();
 }
 
 /**
