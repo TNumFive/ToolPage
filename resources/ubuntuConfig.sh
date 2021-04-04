fa5213ece7e6892168be9ba6bda0f47120c483e9
TNumFive <33838935+TNumFive@users.noreply.github.com>
2021-04-04 21:37:35 +0800

    Update ubuntuConfig.sh

--- resources/ubuntuConfig.sh
+++ resources/ubuntuConfig.sh
@@ -44,7 +44,7 @@ sudo apt install curl -y
 
 printf '获取node version manager\n'
 #安装nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
